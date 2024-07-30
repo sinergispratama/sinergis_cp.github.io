e8443326375c3778c4366f2dd586eedae97940ca
sinergispratama <sinergispratama23@gmail.com>
2024-07-30 14:24:49 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://151.106.113.127/company-profile/';
+$config['base_url'] = 'https://151.106.113.127/company-profile/';
 
 /*
 |--------------------------------------------------------------------------
